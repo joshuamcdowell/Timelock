0827fda4a5fb1ad6fecc1ffd2eb2b8afebcc8d60
joshuamcdowell <kingjaam@gmail.com>
2019-04-30 12:32:00 -0500

    added comments, created interval variable

--- timelock.py
+++ timelock.py
@@ -11,7 +11,12 @@ from pytz import timezone # pip install pytz
 import pytz
 import hashlib
 
+# Interval time in seconds
+INTERVAL = 60
+
+# Get epoch time from epoch text file
 epoch = sys.stdin.read()
+# Parse through text file to get time values
 i = 0
 nextNum = ""
 for ch in epoch:
@@ -42,26 +47,37 @@ if i < 5:
     print("Not enough given. Exiting...")
     exit()
 
+# Set desired timezone
 localTZ = pytz.timezone('America/Chicago')
 targetTZ = pytz.timezone('UTC')
 
+# Set datetime for epoch time
 dt = datetime.datetime(int(epochYear), int(epochMonth), int(epochDay), int(epochHour), int(epochMin), int(epochSec))
 dt = localTZ.localize(dt)
 dt = targetTZ.normalize(dt)
 
-ct = datetime.datetime.now()
+# Set datetime for current time
+# ***** Manually set current time here *****
+ct = datetime.datetime(2017, 04, 26, 15, 14, 30)
+
+# Automatically get current time
+#ct = datetime.datetime.now()
+
 ct = localTZ.localize(ct)
 ct = targetTZ.normalize(ct)
 
+# Calculate difference in time
 finalTime = ct - dt
 ft = int(finalTime.total_seconds())
-ft -= (ft % 30)
+ft -= (ft % INTERVAL)
 
-
+# Hash twice
 h = str(hashlib.md5(str(hashlib.md5(str(ft)).hexdigest())).hexdigest())
+
+# Get the first 2 letters of hash from left to right
+# Get the last 2 numbers of hash from right to left
 numcount = 0
 letcount = 0
-
 for c in h:
     if(numcount >= 2 and letcount >= 2):
         break
@@ -71,6 +87,7 @@ for c in h:
     elif(ord(c) >= 97 and ord(c) <= 122):
         letcount += 1
 
+# There is a small chance that at least 2 letters or 2 numbers will not be generated from the hash
 if(numcount < 2):
     letcount = 4 - numcount
 elif(letcount < 2):
@@ -79,6 +96,7 @@ else:
     numcount = 2
     letcount = 2
 
+# Create code from hash
 code = ""
 k = letcount
 for c in h:
@@ -103,4 +121,5 @@ while(i >= 0):
 
     i -= 1
 
+# Display code
 print(code)
